2f574ebaa8ef5b7882d4bc6c1266074b76f62e83
Berkan Serbes <berkan_serbes@hotmail.com>
2023-01-07 19:29:56 +0300

    instant-forecast-app folder added

---
+++ instant-forecast-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='mainContainer'>
+    <input 
+      type= "text"
+      placeholder= "Search City ..."
+      className= "search"
+      
+    />
+      
+    </div>
+  );
+}
+
+export default App;
