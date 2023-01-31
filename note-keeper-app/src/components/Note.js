f5bde4976fd24e6e984f53df9c0f0b916bea46fb
Berkan Serbes <berkan_serbes@hotmail.com>
2023-01-31 12:17:20 +0300

    Modified Note.js

--- note-keeper-app/src/components/Note.js
+++ note-keeper-app/src/components/Note.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function Note() {
-  return <div>Note</div>;
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
