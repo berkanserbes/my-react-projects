539585defb8650a20b29270056233f7cd41b8c13
Berkan Serbes <berkan_serbes@hotmail.com>
2023-01-31 11:52:51 +0300

    Initial commit > note-keeper-app

---
+++ note-keeper-app/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Header";
+import CreateArea from "./CreateArea";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ note-keeper-app/src/components/CreateArea.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateArea() {
+  return <div>CreateArea</div>;
+}
+
+export default CreateArea;
