323030796df17b7703a4337158e6a11f0e699e33
Berkan Serbes <berkan_serbes@hotmail.com>
2023-03-27 18:07:20 +0300

    Modified HomePage.js

--- favorite-food-app/src/pages/homepage/HomePage.js
+++ favorite-food-app/src/pages/homepage/HomePage.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Search from "../../components/search/Search";
-//import Favorites from "../../components/favorites/Favorites";
+import Favorites from "../../components/favorites/Favorites";
 import RecipeItem from "../../components/recipe-item/RecipeItem";
 import "./style.css";
 
 function HomePage() {
   const [data, setData] = useState([]);
-  // const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  //const [loading, setLoading] = useState(false);
+  const [favoriteQuery, setFavoriteQuery] = useState("");
 
   const handleSubmit = (e, query) => {
     e.preventDefault();
@@ -23,13 +25,55 @@ function HomePage() {
     getRecipes();
   };
 
+  // Create addFavorite function
+  const addFavorite = (item) => {
+    const newFavorites = [...favorites, item];
+    setFavorites(newFavorites);
+  };
+
+  // Create removeFavorite function
+  const removeFavorite = (item) => {
+    const newFavorites = favorites.filter(
+      (favorite) => favorite.id !== item.id
+    );
+    setFavorites(newFavorites);
+  };
+
   return (
     <div>
       <Search handleSubmit={handleSubmit} />
+      <div className="favorites-wrapper">
+        <h1 className="favorites-title">Favorites</h1>
+        <div className="search-favorites">
+          <input
+            name="filterfavorites"
+            placeholder="Search in favorites"
+            value={favoriteQuery}
+            onChange={(e) => setFavoriteQuery(e.target.value)}
+          />
+        </div>
+        <div className="favorites">
+          {favorites.map((item) => {
+            return (
+              <Favorites
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                removeFavorite={() => removeFavorite(item)}
+              />
+            );
+          })}
+        </div>
+      </div>
       <div className="items">
         {data.map((item) => {
           return (
-            <RecipeItem id={item.id} image={item.image} title={item.title} />
+            <RecipeItem
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              addFavorite={() => addFavorite(item)}
+            />
           );
         })}
       </div>
