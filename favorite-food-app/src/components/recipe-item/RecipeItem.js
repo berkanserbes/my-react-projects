6cf764aab04237cbfe00725acb6445dd941484c3
Berkan Serbes <berkan_serbes@hotmail.com>
2023-03-27 18:06:19 +0300

    Added add Favorite button functionality

--- favorite-food-app/src/components/recipe-item/RecipeItem.js
+++ favorite-food-app/src/components/recipe-item/RecipeItem.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css";
 
-function RecipeItem({ id, image, title }) {
+function RecipeItem({ id, image, title, addFavorite }) {
   return (
     <div className="recipe-item" key={id}>
       <div>
         <img src={image} alt="..." />
       </div>
       <p>{title}</p>
-      <button>Add Favorite</button>
+      <button onClick={addFavorite}>Add Favorite</button>
     </div>
   );
 }
